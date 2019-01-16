5fba87270feb937e9d1ce33e19c16568b1a4f643
Roy <roygufo@gmail.com>
2019-01-16 17:27:05 -0600

    Create ViewModel

--- app/src/main/java/com/example/roy/jetpackexample/StartViewModel.kt
+++ app/src/main/java/com/example/roy/jetpackexample/StartViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.roy.jetpackexample
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class StartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _data = MutableLiveData<String>()
+    val data: LiveData<String>
+        get() = _data
+
+    init {
+        _data.value = "Hello, JetPack"
+    }
 }
